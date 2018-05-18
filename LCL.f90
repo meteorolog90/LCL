45896ecc53182829ba60b982167e611fbfd26cc4
meteorolog90 <38101274+meteorolog90@users.noreply.github.com>
2018-05-18 15:10:09 +0200

    Add files via upload

---
+++ LCL.f90
@@ -0,0 +1,57 @@
+program lcl
+implicit none
+
+
+integer :: i,j
+integer, parameter :: N = 45623
+real, dimension(N) :: Soil_aver, Soil_min, Soil_max, Padavine,&
+                      tsr,tmin,tmax,rvsr,Dew_aver, Dew_min,eto,mpvptsr,&
+                      mpvptmax,mpvptmin,ppvptsr,ppvptmin,ppvptmax
+real, dimension(N) :: gama=0.786,alfa,beta,zlcl,zk,k,Tlcl,lclB,zo=1,Gs=0.0065
+
+integer, dimension(N) :: Battery, Leaf_Wetness, Water1, Water2, Water3, &
+                         Water4, Water5, Water6
+
+!mpvp - max.pritisak vodene pare
+!rvsr - relativna vlaznost
+!tsr,tmin,tmax - srednja,min,max temperatrua
+!ppvp - parcijalni pritisak vodene pare
+
+
+10    FORMAT (f6.1,f6.1,f6.1)
+
+open (unit = 10, file = "Senta.csv", status="old")
+open (unit = 11, file = "lcl.csv")
+
+
+do i = 1, N
+
+read (10,*) Soil_aver(i),Soil_min(i),Soil_max(i),Padavine(i),Battery(i),&
+            Leaf_Wetness(i),Water1(i),Water2(i),Water3(i),Water4(i),&
+            Water5(i),Water6(i),tsr(i),tmin(i),tmax(i),&
+            rvsr(i),Dew_aver(i),Dew_min(i)!eto(i)
+
+
+
+
+
+zlcl(i) = 125*(tsr(i)-Dew_aver(i))
+zk(i) = 4000*((100-rvsr(i))/(100+rvsr(i)))
+
+!BARNES METOD
+
+k(i) = 0.001296*Dew_aver(i) + 0.1963
+Tlcl(i) = Dew_aver(i)-k(i)*(tsr(i)-Dew_aver(i))
+
+lclB(i) = zo(i)+(tsr(i)-Tlcl(i))/Gs(i)
+
+
+
+
+
+write (11,*) zlcl(i),zk(i),lclB(i)
+
+
+
+end do
+end program lcl
